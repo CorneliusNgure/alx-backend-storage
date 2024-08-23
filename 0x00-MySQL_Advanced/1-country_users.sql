08bd22910336fb45b67528a6467c278e031d75eb
CorneliusNgure <kingcornelius07@gmail.com>
2024-08-23 08:11:03 -0400

    Task 1

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,8 @@
+-- Creates users table with ENUM of countries
+
+CREATE TABLE IF NOT EXISTS users (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	email VARCHAR(255) NOT NULLL UNIQUE,
+	name VARCHAR(255),
+	country ENUM("US", "CO", "TN") NOT NULL DEFAULT "US"
+	);
