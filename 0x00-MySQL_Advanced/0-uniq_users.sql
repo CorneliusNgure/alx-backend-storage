3ad2c440a38428e194ce58c1f03dbebc8d223eb9
CorneliusNgure <kingcornelius07@gmail.com>
2024-08-23 07:10:55 -0400

    Script to create a table, users

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,6 @@
 --check if the table exits and create one if it donesn't
 
-CREATE TABLE IF NOT EXISTS user (
+CREATE TABLE IF NOT EXISTS users (
 	id INT NOT NULL AUTO_INCREMENT,
 	email VARCHAR(255) NOT NULL UNIQUE,
 	name VARCHAR(255),
