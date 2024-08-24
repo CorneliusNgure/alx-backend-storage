a4e45067ba38c2c12db2ee350cd84de28a62c647
CorneliusNgure <kingcornelius07@gmail.com>
2024-08-24 05:45:07 -0400

    Task 4

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,7 +1,7 @@
 -- Trigger that decreases items after an order is made
 
 CREATE TRIGGER after_adding_order
-AFTER INSERT ON oders
+AFTER INSERT ON orders
 FOR EACH ROW
 UPDATE items
 SET quantity = quantity - NEW.number
