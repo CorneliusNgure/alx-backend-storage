ddb0a73022280186bb360d669aa221968402afa9
CorneliusNgure <kingcornelius07@gmail.com>
2024-08-27 23:42:09 -0400

    9-insert_school.py

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,16 @@
+from pymongo import MongoClient
+"""Insert a document"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """
+    Inserts a new document into the specified MongoDB collection.
+
+    :mongo_collection: pymongo collection object
+    :kwargs: Fields and values for the new document
+    :return: The _id of the inserted document
+    """
+
+    result = mongo_collection.insert_one(kwargs)
+
+    return result.inserted_id
