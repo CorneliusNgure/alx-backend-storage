c885d72d319271d6fbcb39eb97070f177f0139b1
CorneliusNgure <kingcornelius07@gmail.com>
2024-08-28 22:34:57 -0400

    Incrementing values

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -6,7 +6,8 @@ Redis database.
 
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable, Optional
+from functools import wraps
 
 
 def count_calls(method: Callable) -> Callable:
@@ -68,6 +69,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
         Stores the provided data in the Redis database with
