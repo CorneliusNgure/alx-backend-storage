97842c28daa5ac316e3a0b937c8d0001761e6903
CorneliusNgure <kingcornelius07@gmail.com>
2024-08-28 21:53:56 -0400

    Reading from Redis and recovering original typeC

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -50,3 +50,55 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[Callable[[bytes], Union[
+        str, bytes, int, float]]] = None) -> Optional[Union[
+            str, bytes, int, float]]:
+        """
+        Retrieves data from the Redis database using the given key.
+        An optional Callable can be passed to convert the data
+        back to the desired format.
+
+        Args:
+            key(str): Key used to retrieve the data from Redis.
+            fn (Optional[Callable[[bytes], Union[str, bytes,
+            int, float]]]): A callable to convert the data.
+            Defaults to None, returning the raw byte string.
+
+        Returns:
+            Optional[Union[str, bytes, int, float]]: The data
+            retrieved from Redis.
+            Returns None if the key does not exist.
+        """
+        data = self._redis.get(key)
+        if data is None:
+            return None
+        if fn:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> Optional[str]:
+        """
+        Retrieves data from Redis and converts it to UTF-8 string.
+
+        Args:
+            key(str): The key used to retrieve the data from Redis.
+
+        Returns:
+            Optional[str]: Data converted to a string,
+            or None if the key does not exist.
+        """
+        return self.get(key, lambda d: d.decode('utf-8'))
+
+    def get_int(self, key: str) -> Optional[int]:
+        """
+        Retrieves data from Redis and converts it to an int.
+
+        Args:
+            key(str): Key used to retrieve the data from Redis.
+
+        Returns:
+            Optional[int]: Data converted to an int,
+            or None if the key does not exist.
+        """
+        return self.get(key, lambda d: int(d))
