8a0355f5ad64b6a2d396f6aa6992e128aef8564f
CorneliusNgure <kingcornelius07@gmail.com>
2024-08-30 04:13:23 -0400

    Log stats about Nginx stored in MongoDB

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+from pymongo import MongoClient
+
+
+def nginx_stats():
+    client = MongoClient('mongodb://localhost:27017/')
+    db = client.logs
+    collection = db.nginx
+
+    log_count = collection.count_documents({})
+    print(f"{log_count} logs")
+
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    print("Methods:")
+    for method in methods:
+        method_count = collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {method_count}")
+
+    status_count = collection.count_documents({
+        "method": "GET", "path": "/status"
+        })
+    print(f"{status_count} status check")
+
+
+if __name__ == "__main__":
+    nginx_stats()
